e4c8eb2c538379dda34eb9809b26ff7e5db0ddeb
bartektroszka <bartek99onet.com.pl@onet.pl>
2021-09-07 20:13:08 +0200

    basic EditorConsole

---
+++ frontend/solve-conflicts/src/components/utils/editorConsole/EditorConsole.style.ts
@@ -0,0 +1,19 @@
+import { ContainerProps } from "./types";
+import styled from "styled-components";
+
+export const $EditorConsoleContainer = styled.div<ContainerProps>`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  padding: 0.5rem;
+  display: flex;
+  gap: 0.5rem;
+  flex-direction: row-reverse;
+  background-color: grey;
+`;
+
+export const $ConsoleContainer = styled.div`
+  width: 30%;
+  height: 100%;
+  display: flex;
+  overflow: hidden;
+`;
