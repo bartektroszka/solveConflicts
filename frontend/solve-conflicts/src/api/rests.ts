cb2a7e53c8074fe7b6a87fe4d7c312fd28698157
bartektroszka <bartek99onet.com.pl@onet.pl>
2021-11-08 23:38:05 +0100

    rest udpate

--- frontend/solve-conflicts/src/api/rests.ts
+++ frontend/solve-conflicts/src/api/rests.ts
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 const url = '127.0.0.1:5000';
 
-export const execute = (cmd: string) =>
-  axios({
-    method: 'post',
-    url: `${url}/execute`,
-    data: cmd,
-  });
+const api = axios.create({
+  baseURL: url,
+});
+export const execute = (cmd: string) => api.post('/execute', cmd);
