b8118e52faa10077667d905d4734bbf6ccb5bd79
bartektroszka <bartek99onet.com.pl@onet.pl>
2021-11-09 00:28:06 +0100

    error fix

--- src/components/utils/popup/Popup.tsx
+++ src/components/utils/popup/Popup.tsx
@@ -1,19 +1,18 @@
-import { Props } from "./types";
-import { $Popup, $Notification, $ButtonContainer } from "./Popup.style";
-import { Button } from "../button/Button";
+import { Props } from './types';
+import { $Popup, $Notification, $ButtonContainer } from './Popup.style';
+import { Button } from '../button/Button';
 const Popup = ({ width, height, children, open, setOpen }: Props) => {
-  return open ? 
-      (<$Popup>
-        <$Notification width={width} height={height}>
-          {children}
-          
+  return open ? (
+    <$Popup>
+      <$Notification width={width} height={height}>
+        {children}
+
         <$ButtonContainer>
-          <Button onClick={() => setOpen(!open)}></Button> 
+          <Button buttonText='CLOSE' onClick={() => setOpen(!open)}></Button>
         </$ButtonContainer>
-        </$Notification>
-      
-    </$Popup>)
-    : null
+      </$Notification>
+    </$Popup>
+  ) : null;
 };
 
 export default Popup;
