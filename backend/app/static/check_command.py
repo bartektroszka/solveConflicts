2f9d00eb3cc949820c2255e5bf3be24f0a680380
Marcin <marcin.knapik37@gmail.com>
2021-11-29 20:44:56 +0100

    Dodanie ciasteczka 'session'

--- backend/app/static/check_command.py
+++ backend/app/static/check_command.py
@@ -9,9 +9,4 @@ def valid_command(line):
     words = [word for word in words if len(word) > 0]
     print("WORDS: ", words)
 
-    return len(words) > 0 and words[0] == 'git'
-
-
-if __name__ == '__main__':
-    command_to_check = "\tgit\t\thelloe    fjnds "
-    print("GOOD " if valid_command(command_to_check) else "BAD")
+    return len(words) > 0 and words[0] == 'git' and any(['|' in word for word in words])
