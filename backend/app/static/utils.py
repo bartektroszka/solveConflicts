4971b736bfc2d5832deda3f0b466f00b43bc754c
Marcin <marcin.knapik37@gmail.com>
2021-12-02 18:12:37 +0100

    Small fixes and README update

--- backend/app/static/utils.py
+++ backend/app/static/utils.py
@@ -1,5 +1,5 @@
 import random
-
+import os
 
 def is_nick(name):
     alphabet = 'abcdefghijklmnopqrstuvwxyz_0123456789'
@@ -9,3 +9,8 @@ def is_nick(name):
 def random_id():
     alphabet = 'abcdefghijklmnopqrstuvwxyz_0123456789'
     return ''.join([random.choice(alphabet) for _ in range(10)])
+
+
+def run_command(command):
+    print("RUNNING COMMAND")
+    print(f"{command}: \033[31m{os.popen(command).read()}\033[m")
