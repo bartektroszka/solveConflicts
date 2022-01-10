ec55d2d09ac2a662883f13093d9b028a09e00808
bartektroszka <bartek99onet.com.pl@onet.pl>
2022-01-10 23:45:03 +0100

    empty commit messages and authors

--- frontend/solve-conflicts/src/components/utils/gitTree/GitTree.tsx
+++ frontend/solve-conflicts/src/components/utils/gitTree/GitTree.tsx
@@ -5,21 +5,6 @@ export const GitTree = ({ commits }: Props) => {
   return (
     <Gitgraph>
       {(gitgraph) => {
-        /* const master = gitgraph.branch({
-          name: 'master',
-          style: {
-            color: 'green',
-            label: {
-              bgColor: '#ffce52',
-              color: 'black',
-              strokeColor: '#ce9b00',
-            },
-          },
-        });
-        const father: GitCommit = commits.filter(
-          (commit) => commit.parents.length === 0
-        )[0]; */
-        const names: any = {};
         const futureBranches: any = {};
         const nativeBranch: any = {};
 
@@ -29,34 +14,96 @@ export const GitTree = ({ commits }: Props) => {
             commit.parents.includes(tempFather.hash)
           );
           let parent = futureBranches[tempFather.hash];
-          
-          // looping from one, since the first children will be from native 
+
+          // looping from one, since the first children will be from native
           for (var i = 1; i < children.length; i++) {
             parent[children[i].hash] = gitgraph.branch(children[i].hash);
           }
         };
         const buildCommit = (commit: GitCommit) => {
           if (commit.parents.length === 0) {
-            nativeBranch[commit.hash] = gitgraph.branch('master')
-            nativeBranch[commit.hash].commit();
-          } 
-          else if (commit.parents.length === 1) {
+            nativeBranch[commit.hash] = gitgraph.branch({
+              name: 'master',
+              style: {
+                color: 'green',
+                label: {
+                  bgColor: '#ffce52',
+                  color: 'black',
+                  strokeColor: '#ce9b00',
+                },
+              },
+            });
+            nativeBranch[commit.hash].commit(
+              nativeBranch[commit.hash].name === 'master'
+                ? {
+                    subject: '',
+                    body: '',
+                    dotText: '',
+                    style: {
+                      dot: { color: 'green' },
+                      message: { color: 'green', displayAuthor: false },
+                    },
+                  }
+                : {
+                    subject: '',
+                    body: '',
+                    style: { message: { displayAuthor: false } },
+                  }
+            );
+          } else if (commit.parents.length === 1) {
             let branch;
             if (commit.hash in futureBranches[commit.parents[0]])
               branch = futureBranches[commit.parents[0]][commit.hash];
-            else
-              branch = nativeBranch[commit.parents[0]];
-            branch.commit(commit.hash);
-            nativeBranch[commit.hash] = branch
-          } 
-          else if (commit.parents.length === 2) {
-            let mainBranch = nativeBranch[commit.parents[0]]; 
+            else branch = nativeBranch[commit.parents[0]];
+            branch.commit(
+              branch.name === 'master'
+                ? {
+                    subject: '',
+                    body: '',
+                    dotText: '',
+                    style: {
+                      dot: { color: 'green' },
+                      message: { color: 'green', displayAuthor: false },
+                    },
+                  }
+                : {
+                    subject: '',
+                    style: {
+                      message: { displayAuthor: false },
+                    },
+                  }
+            );
+            nativeBranch[commit.hash] = branch;
+          } else if (commit.parents.length === 2) {
+            let mainBranch = nativeBranch[commit.parents[0]];
             let secondBranch = nativeBranch[commit.parents[1]];
-            
+
             // mainBranch.commit();
             // secondBranch.commit();
-            mainBranch.merge(secondBranch);
-            nativeBranch[commit.hash] = mainBranch
+
+            mainBranch.merge({
+              branch: secondBranch,
+              commitOptions:
+                mainBranch.name === 'master'
+                  ? {
+                      style: {
+                        subject: '',
+                        body: '',
+                        dotText: '',
+                        dot: { color: 'green' },
+                        message: { color: 'green', displayAuthor: false },
+                      },
+                    }
+                  : {
+                      style: {
+                        subject: '',
+                        body: '',
+                        dotText: '',
+                        message: { displayAuthor: false },
+                      },
+                    },
+            });
+            nativeBranch[commit.hash] = mainBranch;
           }
         };
         commits.forEach((commit) => {
