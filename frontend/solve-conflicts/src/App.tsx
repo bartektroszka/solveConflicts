f472f7bbb964ec7f7be920dce6ca61ff11f8bfff
bartektroszka <bartek99onet.com.pl@onet.pl>
2022-01-19 00:27:36 +0100

    further levels

--- frontend/solve-conflicts/src/App.tsx
+++ frontend/solve-conflicts/src/App.tsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react';
 import { getcurrentLevel, getFolderTree } from './api/rests';
 import { $App } from './App.style';
+import LevelFour from './components/levels/levelFour/LevelFour';
 import LevelOne from './components/levels/levelOne/LevelOne';
+import LevelThree from './components/levels/levelThree/levelThree';
 import LevelTwo from './components/levels/levelTwo/LevelTwo';
 import LevelBar from './components/utils/levelBar/LevelBar';
+import Popup from './components/utils/popup/Popup';
 
 function App() {
   const [currentLevel, setCurrentLevel] = useState(1);
+  const [reset, setReset] = useState(false);
   useEffect(() => {
     getcurrentLevel().then((response: any) => {
       setCurrentLevel(response.data.level);
@@ -15,25 +19,52 @@ function App() {
   const levels: { [key: number]: React.ReactNode } = {
     1: (
       <LevelOne
-        title='level 1'
         setLevel={(levelNumber: number) => {
           setCurrentLevel(levelNumber);
         }}
+        reset={() => setReset(true)}
       />
     ),
     2: (
       <LevelTwo
-        title='level 2'
         setLevel={(levelNumber: number) => {
           setCurrentLevel(levelNumber);
         }}
+        reset={() => setReset(true)}
       />
     ),
-    3: null,
+    3: (
+      <LevelThree
+        setLevel={(levelNumber: number) => {
+          setCurrentLevel(levelNumber);
+        }}
+        reset={() => setReset(true)}
+      />
+    ),
+    4: (
+      <LevelFour
+        setLevel={(levelNumber: number) => {
+          setCurrentLevel(levelNumber);
+        }}
+        reset={() => setReset(true)}
+      />
+    ),
   };
   return (
     <$App>
       {levels[currentLevel]}
+      <Popup
+        open={reset}
+        buttonText='RESETUJ'
+        afterClose={() => {
+          setReset(false);
+          window.location.reload();
+        }}
+        width='300px'
+        height='200px'
+      >
+        Niestety, nabałaganiłeś i musisz zresetować poziom.
+      </Popup>
       <LevelBar numberOfLevels={8} currentLevel={currentLevel}></LevelBar>
     </$App>
   );
