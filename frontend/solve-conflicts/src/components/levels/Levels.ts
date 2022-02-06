8b5b353915391ce92a571cd0951756cd050c2d15
bartektroszka <bartek99onet.com.pl@onet.pl>
2022-02-06 14:05:31 +0100

    popup sizes corrected

--- frontend/solve-conflicts/src/components/levels/Levels.ts
+++ frontend/solve-conflicts/src/components/levels/Levels.ts
@@ -4,8 +4,8 @@ export const levels = [
       message: `Witaj w solveConflicts! Mamy nadzieję, że
       wspólnie nauczymy się czegoś ciekawego o rozwiązywaniu
       konfliktów w systemie kontroli wersji GIT.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '200px',
       stage: 1,
     },
     {
@@ -14,8 +14,8 @@ export const levels = [
       napisał własną wersję tego jakich proporcji należy użyć do ciasta.
       Musisz teraz zadecydować, który przepis jest lepszy i zostanie opublikowany.
       Rozpocznij od wpisania komendy: ‘git branch‘.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '300px',
       stage: 1,
     },
     {
@@ -24,8 +24,8 @@ export const levels = [
       gałęzi (friend branch). Spróbuj automatycznie połączyć te dwie
       gałęzie przy użyciu komendy: git merge friend_branch”
       W razie potrzeby pamiętaj o komendach: ‘help’ oraz ‘hint’.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '300px',
       stage: 1,
     },
     {
@@ -35,8 +35,8 @@ export const levels = [
       oddzielonymi sekcjami. Sekcja HEAD oznacza wersję Twojej gałęzi. Druga
       sekcja to zmiany kolegi. Twoim zadaniem jest zmodyfikować ten plik wybierając
       jedną z dwóch wersji, po czym użyć komendy ‘git add’ a następnie dokończyć merge.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '300px',
       stage: 2,
     },
   ],
@@ -47,8 +47,8 @@ export const levels = [
       stylowania Waszej strony internetowej. W tym pliku są trzy sekcje (“header”,
       “main-table” oraz “footer”). Rozpocznij przejrzenia zawartości pliku oraz
       próby zmergowania tych gałęzi!`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '250px',
       stage: 1,
     },
     {
@@ -57,8 +57,8 @@ export const levels = [
       z kolegą ustaliliście, aby zachować Twoją wersję kawałka odpowiadającego
       za sekcję “footer”, ale wersję kolegi jeżeli chodzi o “header”.
       Do dzieła!`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '250px',
       stage: 1,
     },
   ],
@@ -68,8 +68,8 @@ export const levels = [
       konflikt może wystąpić jedynie przy próbie wykonania ‘git merge’? Okazuje
       się, że konflikty mogą pojawiać się również przy innych komendach
       zmieniających strukturę repozytorium.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '250px',
       stage: 1,
     },
     {
@@ -79,15 +79,15 @@ export const levels = [
       Postanowiłeś, że możesz połączyć
       ten plik w jeden i sprawić żeby historia wyglądała tak, jakbyś nigdy nie rozdzielał pracy na dwoje.
       Spróbuj wykonać komendę ‘git rebase liczby_catalana’.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '300px',
       stage: 1,
     },
     {
       message: `Funkcja silnia posiada dwie różne implementacje. Wybierz tę, z której wolałbyś korzystać, a następnie ‘pogódź’ resztę pliku zachowując ukończone
       implementacje pozostałych dwóch funkcji. Po wszystkim użyj komendy ‘git rebase’ z flagą ‘–continue’.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '150px',
       stage: 2,
     },
   ],
@@ -98,7 +98,7 @@ export const levels = [
       strategię na rozwiązanie problemu optymalizacyjnego, a po jakimś czasie
       i naradach drużyna przechodzi w tryb implementacji kodu.`,
       width: `600px`,
-      height: `300px`,
+      height: `200px`,
       stage: 1,
     },
     {
@@ -110,29 +110,29 @@ export const levels = [
       istotny jest tylko jeden z nich o nazwie “defines”. W tym komicie jest
       kilka przydatnych linii kodu, których nie ma sensu przepisywać
       automatycznie.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '300px',
       stage: 1,
     },
     {
       message: `Za pomocą “git cherry-pick COMMIT” wyłuskaj komit, którego
       potrzebujesz i dołącz go do swojej gałęzi, a jeżeli w międzyczasie pojawi
       się również jakiś konflikt, to go rozwiąż!`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '150px',
       stage: 1,
     },
     {
       message: `A niech to! Znowu jeden z tych przeklętych konfliktów! Na pewno dasz sobie z nim radę.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '120px',
       stage: 2,
     },
     {
       message: `Może wydawać się to dziwne, ale jedna z sekcji powstałych w pliku
       konfliktowym jest pusta, ale oznacza, że git oznacza brak zmian, jako zmianę.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '150px',
       stage: 2,
     },
   ],
@@ -145,8 +145,8 @@ export const levels = [
       w trakcie pracy, Twój szef powiedział, żeby się jednak wstrzymać
       z mergowaniem, bo nie jest jeszcze do końca pewien, które wersje plików
       chciałby zostawić.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '300px',
       stage: 1,
     },
     {
@@ -155,16 +155,16 @@ export const levels = [
       i próbować wrócić do prac na jednej z gałęzi. Jak to zrobić? Okazuje się,
       że sprawa jest bardzo prosta. Wystarczy wykonać komendę “git merge --abort”,
       aby repozytorium powróciło do stanu sprzed wywołania komendy merge.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '300px',
       stage: 1,
     },
     {
       message: `Okazuje się, że flaga --abort działa
       w bardzo podobny sposób, jeżeli chcielibyśmy cofnąć zmiany w trakcie
       ‘git rebase’ albo ‘git cherry-pick’`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '200px',
       stage: 1,
     },
   ],
@@ -174,8 +174,8 @@ export const levels = [
       szczyty. W tym celu postanowiliście zrobić kalendarz wypraw na szlaki.
       Każdy z Was podał jakieś propozycje tras, i teraz pora na połączenie
       pomysłów. Użyj komendy git merge!`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '200px',
       stage: 1,
     },
     {
@@ -184,16 +184,16 @@ export const levels = [
       W takiej sytuacji najlepiej byłoby pozbyć się tych niepotrzebnych zmian
       i wrócić do sytuacji bezpośrednio po zrobieniu ostatniego komita. Na
       szczęście Git daje nam taką możliwość! Z pomocą przychodzi komenda git restore.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '300px',
       stage: 2,
     },
     {
       message: `Użyj tej komendy by, przywrócić prawidłowy stan dla pliku wyjazd.txt,
       a następnie połącza gałęzie ‘pat’ i ‘mat’ (jeżeli zajdzie konflikt, to
       zadbaj o jego rozwiązanie)`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '200px',
       stage: 2,
     },
   ],
@@ -202,16 +202,16 @@ export const levels = [
       message: `Postanowiłeś zrobić swoim rodzicom prezent! Na ich trzydziestą rocznicę
       ślubu chcesz zrobić im remont mieszkania. Nic prostszego… Już nawet
       przygotowałeś arkusz, w którym wpisałeś co chcesz im wyremontować.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '200px',
       stage: 1,
     },
     {
       message: `Jak to jednak w życiu bywa… rodzice po tym jak usłyszeli, że chcesz
       zrobić im prezent, postanowili wprowadzić pewne poprawki do arkusza…
       Stworzyli nawet nową gałąź w systemie GIT by tam notować swoje zmiany.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '300px',
       stage: 1,
     },
     {
@@ -219,23 +219,23 @@ export const levels = [
       poprawek, które wprowadzili rodzice jest zatrważająca. Trudno Ci będzie w
       takiej sytuacji odmówić. Nie wiemy jak uratować Twój portfel, ale możemy
       pokazać Ci metodę na zaoszczędzenie czasu.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '250px',
       stage: 1,
     },
     {
       message: `Dla komendy merge można podać flagę ‘-X’, która pozwala na określenie
       strategii przy merdżowaniu. i tak gdy podamy ‘-X ours’ to wszystkie
       konflikty będą rozstrzygane na korzyść HEAD, a dla ‘-X theirs’ na korzyść drugiej gałęzi.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '200px',
       stage: 1,
     },
     {
       message: `W tym poziomie, możesz albo spróbować wykonać wszystkie zmiany ręcznie
       (zawsze zostawiając wersje z gałęzi rodziców), albo po prostu skorzystać z flagi -X`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '150px',
       stage: 1,
     },
   ],
@@ -245,15 +245,15 @@ export const levels = [
       Na potrzeby naprawy kawałka kodu stworzyłeś gałąź bugFix i dokonałeś
       na niej kilka zmian. Twoim zadaniem jest podłączenie tych zmian
       do głównego nurtu gałęzi. Możesz to zrobić za pomocą ‘merge’, albo ‘rebase’.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '200px',
       stage: 1,
     },
     {
       message: `
       A jednak postał konflikt. Rozwiąż go i odbierz swój wymarzony dyplom ukończenia kursu!.`,
-      width: "600px",
-      height: "300px",
+      width: '600px',
+      height: '120px',
       stage: 2,
     },
   ],
