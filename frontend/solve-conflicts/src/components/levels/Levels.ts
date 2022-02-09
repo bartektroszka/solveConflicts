375503052071e59c7f7452e3f3bb69a876bacca8
bartektroszka <bartek99onet.com.pl@onet.pl>
2022-02-10 00:11:16 +0100

    fixing linguistic

--- frontend/solve-conflicts/src/components/levels/Levels.ts
+++ frontend/solve-conflicts/src/components/levels/Levels.ts
@@ -22,7 +22,7 @@ export const levels = [
       message: `Zauważ, że znajdujesz się aktualnie na gałęzi ‘master’, gdzie
       jest plik z Twoją wersją przepisu. Twój kolega ma przepis na osobnej
       gałęzi (friend branch). Spróbuj automatycznie połączyć te dwie
-      gałęzie przy użyciu komendy: git merge friend_branch”
+      gałęzie przy użyciu komendy: ’git merge friend_branch’
       W razie potrzeby pamiętaj o komendach: ‘help’ oraz ‘hint’.`,
       width: '600px',
       height: '300px',
@@ -31,7 +31,7 @@ export const levels = [
     {
       message: `Niestety… zawartości plików są istotnie różne i GIT nie jest
       w stanie ich automatycznie połączyć. Zauważ, że zawartość pliku
-      ‘przepis.txt’ uległa zmianie. Znajdują się w nim miejsce z dwoma wyraźnie
+      ‘przepis.txt’ uległa zmianie. Znajdują się w nim miejsca z dwoma wyraźnie
       oddzielonymi sekcjami. Sekcja HEAD oznacza wersję Twojej gałęzi. Druga
       sekcja to zmiany kolegi. Twoim zadaniem jest zmodyfikować ten plik wybierając
       jedną z dwóch wersji, po czym użyć komendy ‘git add’ a następnie dokończyć merge.`,
@@ -45,7 +45,7 @@ export const levels = [
       message: `Pierwszy poziom był dość prosty. Teraz będzie nieco trudniej.
       Masz za zadanie połączyć dwie wersje pliku ‘style.json’ z konfiguracją
       stylowania Waszej strony internetowej. W tym pliku są trzy sekcje (“header”,
-      “main-table” oraz “footer”). Rozpocznij przejrzenia zawartości pliku oraz
+      “main-table” oraz “footer”). Rozpocznij od przejrzenia zawartości pliku oraz
       próby zmergowania tych gałęzi!`,
       width: '600px',
       height: '250px',
@@ -73,8 +73,8 @@ export const levels = [
       stage: 1,
     },
     {
-      message: `Jedną z nich jest git rebase. Pomoże nam ono uporządkować historię
-      naszego repozytorium. Pracowałeś ostatnio nad dwiema gałęźmi, z których jedna
+      message: `Jedną z nich jest ‘git rebase’. Pomoże nam ona uporządkować historię
+      naszego repozytorium. Pracowałeś ostatnio nad dwiema gałęziami, z których jedna
       wyliczała liczby Catalana, a na drugiej umieściłeś kod szeregu Taylora.
       Postanowiłeś, że możesz połączyć
       ten plik w jeden i sprawić żeby historia wyglądała tak, jakbyś nigdy nie rozdzielał pracy na dwoje.
@@ -117,7 +117,7 @@ export const levels = [
     {
       message: `Za pomocą “git cherry-pick COMMIT” wyłuskaj komit, którego
       potrzebujesz i dołącz go do swojej gałęzi, a jeżeli w międzyczasie pojawi
-      się również jakiś konflikt, to go rozwiąż!`,
+      się również jakiś konflikt, rozwiąż go!`,
       width: '600px',
       height: '150px',
       stage: 1,
@@ -129,8 +129,8 @@ export const levels = [
       stage: 2,
     },
     {
-      message: `Może wydawać się to dziwne, ale jedna z sekcji powstałych w pliku
-      konfliktowym jest pusta, ale oznacza, że git oznacza brak zmian, jako zmianę.`,
+      message: `Jedna z części stanowiących konflikt jest pusta. 
+      Oznacza to mniej więcej tyle, że Git nie jest pewny czy chcesz w tym miejscu zastosować zmiany z drugiego komita.`,
       width: '600px',
       height: '150px',
       stage: 2,
@@ -138,11 +138,11 @@ export const levels = [
   ],
   [
     {
-      message: `Ten poziom, będzie krótką odskocznią od rozwiązywania konfliktów, ale
-      też niesie w sobie nieco przydatnej wiedzy. Wyobraź sobie sytuację, gdzie
-      w pracy dostałeś zlecenie na wykonanie połączenie dwóch (lub więcej)
+      message: `Ten poziom będzie krótką odskocznią od rozwiązywania konfliktów,
+      jednak niesie on ze sobą bardzo przydatną wiedzę. Wyobraź sobie sytuację, gdzie
+      w pracy dostałeś zlecenie na wykonanie połączenia dwóch (lub więcej)
       bardzo rozbudowanych gałęzi. Rozwiązałeś już część konfliktów, ale
-      w trakcie pracy, Twój szef powiedział, żeby się jednak wstrzymać
+      w trakcie pracy Twój szef powiedział, żeby się jednak wstrzymać
       z mergowaniem, bo nie jest jeszcze do końca pewien, które wersje plików
       chciałby zostawić.`,
       width: '600px',
@@ -162,7 +162,7 @@ export const levels = [
     {
       message: `Okazuje się, że flaga --abort działa
       w bardzo podobny sposób, jeżeli chcielibyśmy cofnąć zmiany w trakcie
-      ‘git rebase’ albo ‘git cherry-pick’`,
+      ‘git rebase’ albo ‘git cherry-pick’.`,
       width: '600px',
       height: '200px',
       stage: 1,
@@ -170,28 +170,28 @@ export const levels = [
   ],
   [
     {
-      message: `Razem z bratem planujecie wyjechać latem w góry pozdobywać Tatrzańskie
+      message: `Razem z bratem planujecie wyjechać latem w góry pozdobywać tatrzańskie
       szczyty. W tym celu postanowiliście zrobić kalendarz wypraw na szlaki.
-      Każdy z Was podał jakieś propozycje tras, i teraz pora na połączenie
+      Każdy z Was podał jakieś propozycje tras i teraz pora na połączenie
       pomysłów. Użyj komendy git merge!`,
       width: '600px',
       height: '200px',
       stage: 1,
     },
     {
-      message: `Niestety okazuje się, że merge się nawet nie rozpoczął… bo z pliku
+      message: `Niestety okazuje się, że merge się nawet nie rozpoczął… bo w pliku
       ‘wyjazd.txt’ znalazła się jakaś przypadkowa zmiana od ostatniego komita.
       W takiej sytuacji najlepiej byłoby pozbyć się tych niepotrzebnych zmian
       i wrócić do sytuacji bezpośrednio po zrobieniu ostatniego komita. Na
-      szczęście Git daje nam taką możliwość! Z pomocą przychodzi komenda git restore.`,
+      szczęście Git daje nam taką możliwość! Z pomocą przychodzi komenda ‘git restore’.`,
       width: '600px',
       height: '300px',
       stage: 2,
     },
     {
-      message: `Użyj tej komendy by, przywrócić prawidłowy stan dla pliku wyjazd.txt,
-      a następnie połącza gałęzie ‘pat’ i ‘mat’ (jeżeli zajdzie konflikt, to
-      zadbaj o jego rozwiązanie)`,
+      message: `Użyj tej komendy by przywrócić prawidłowy stan dla pliku wyjazd.txt,
+      a następnie połącz gałęzie ‘pat’ i ‘mat’ (jeżeli zajdzie konflikt, to
+      zadbaj o jego rozwiązanie).`,
       width: '600px',
       height: '200px',
       stage: 2,
@@ -200,7 +200,7 @@ export const levels = [
   [
     {
       message: `Postanowiłeś zrobić swoim rodzicom prezent! Na ich trzydziestą rocznicę
-      ślubu chcesz zrobić im remont mieszkania. Nic prostszego… Już nawet
+      ślubu chcesz zrobić im remont mieszkania. Nic prostszego…. Już nawet
       przygotowałeś arkusz, w którym wpisałeś co chcesz im wyremontować.`,
       width: '600px',
       height: '200px',
@@ -225,7 +225,7 @@ export const levels = [
     },
     {
       message: `Dla komendy merge można podać flagę ‘-X’, która pozwala na określenie
-      strategii przy merdżowaniu. i tak gdy podamy ‘-X ours’ to wszystkie
+      strategii przy scalaniu gałęzi. I tak gdy podamy ‘-X ours’ to wszystkie
       konflikty będą rozstrzygane na korzyść HEAD, a dla ‘-X theirs’ na korzyść drugiej gałęzi.`,
       width: '600px',
       height: '200px',
@@ -233,7 +233,7 @@ export const levels = [
     },
     {
       message: `W tym poziomie, możesz albo spróbować wykonać wszystkie zmiany ręcznie
-      (zawsze zostawiając wersje z gałęzi rodziców), albo po prostu skorzystać z flagi -X`,
+      (zawsze zostawiając wersje z gałęzi rodziców), albo skorzystać z flagi -X`,
       width: '600px',
       height: '150px',
       stage: 1,
@@ -244,7 +244,7 @@ export const levels = [
       message: `Prowadzisz prace nad projektem programistycznym na gałęzi ‘develop’.
       Na potrzeby naprawy kawałka kodu stworzyłeś gałąź bugFix i dokonałeś
       na niej kilka zmian. Twoim zadaniem jest podłączenie tych zmian
-      do głównego nurtu gałęzi. Możesz to zrobić za pomocą ‘merge’, albo ‘rebase’.`,
+      do głównego nurtu gałęzi. Możesz to zrobić za pomocą ‘merge’ lub ‘rebase’.`,
       width: '600px',
       height: '200px',
       stage: 1,
@@ -268,14 +268,14 @@ export const tasks = [
   Twoja wersja sekcji “footer” oraz cudza wersja “header”.
   Do wykonania zadania użyj komendy ‘git merge’.`,
   `Nie tylko ‘merge’ powoduje konflikty. Spróbuj użyć ‘git rebase’ do
-  przeniesienia swoich zmian pliku kod.py do gałęzi ‘liczby_catalana’. Napotkasz
+  przeniesienia swoich zmian w pliku kod.py do gałęzi ‘liczby_catalana’. Napotkasz
   konflikt, który trzeba rozwiązać wybierając jedną z dostępnych
   implementacji funkcji ‘silnia’ oraz dokończone implementacje pozostałych
   funkcji.`,
   `Na tym poziomie chcemy zademonstrować działanie komendy “git cheery-pick”
   oraz to, że może ona powodować konflikty. Za jej pomocą dostań się do
   kodu zawartego w komicie “przydatne dyrektywy”. Skorzystaj z całego kodu
-  oferowanego przez ten commit.`,
+  oferowanego przez ten komit.`,
   `Czasami porzucenie zamiaru połączenia gałęzi może być najlepszym wyborem.
   Zwłaszcza gdy nie jest się pewny tego jak rozwiązać konflikty,
   lub jest ich zbyt wiele do rozwiązania w danym momencie. W takim przypadku
@@ -284,7 +284,7 @@ export const tasks = [
   `Bywa tak, że merge nie rozpocznie się, ponieważ lokalnie są obecne pliki,
   które nie zostały dodane do ‘przechowalni’ (staging area). Tak stało się
   również z plikiem ‘wyjazd.txt’. Sprawdź czy po użyciu ‘git restore’, które
-  odrzuci nieskomitowane zminy z tego pliku, sytuacja ulegnie zmianie.`,
+  odrzuci nieskomitowane zmiany z tego pliku, sytuacja ulegnie zmianie.`,
   `Jeżeli wiemy, że konflikty będą rozwiazywane zawsze na korzyść jednej z
   gałęzi, to możemy podać do ‘git merge’ flagę ‘-X’, która określi
   strategię automatycznego rozwiązywania konfliktów. W tym przypadku
